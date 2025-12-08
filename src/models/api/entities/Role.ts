c98812a601058385a296b16f3c6553107bf87c1d
loza.dev <robertoloxa88@gmail.com>
2025-12-07 19:01:31 -0600

    changes

--- src/models/api/entities/Role.ts
+++ src/models/api/entities/Role.ts
@@ -1,5 +1,5 @@
 import { RoleName } from "src/config/roles"
-import type Permissions from "./Permissions"
+import Permissions from "./Permissions"
 
 export default interface Role {
     id?: number
